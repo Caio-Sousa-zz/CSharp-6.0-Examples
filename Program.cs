2d87cab0107f204e8af5869210476c20a3eff3bb
Caio-Sousa <caiyo.projeto@gmail.com>
2018-02-03 18:49:37 -0200

    Include sections

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace C__6._0
 {
@@ -6,15 +7,21 @@ namespace C__6._0
     {
         static void Main(string[] args)
         {
-            
+            /*
             stringInterpolationExample();
             nullConditionalOperatorExample();
             nameOfOperatorExample();
+            exceptionFilterExample();
+            indexInitializer();
+            usingStatic();
+            */
+            autoPropertyInitializer();
         }
 
+        // 1.1 String Interpolation.
         private static void stringInterpolationExample()
         {
-            Console.WriteLine("\nString interpolation example \n");
+            displayTitle("String interpolation example");
             string t = "random variable value";
             Console.WriteLine($"Display variable value: {t}");
             Person p = new Person();
@@ -22,9 +29,10 @@ namespace C__6._0
             Console.WriteLine($"Display name value: {p.name}");
         }
 
+        // 1.2 Null conditional operator example.
         private static void nullConditionalOperatorExample()
         {
-            Console.WriteLine("\nNull conditional operator\n");
+            displayTitle("Null conditional operator");
             Person p = new Person();
             Console.WriteLine($"Display name value: {p?.name ?? "Null Name"}");
             p.name = "John";
@@ -34,20 +42,83 @@ namespace C__6._0
             Console.WriteLine($"Display address value: {p?.address ?? "Null Address"}");
         }
 
-
+        // 1.3 Name of operator example.
         private static void nameOfOperatorExample()
         {
-            Console.WriteLine("\nnameof operator example \n");
+            displayTitle("nameof operator example");
             Console.WriteLine($"Name of Class:{nameof(Person)}");
             Console.WriteLine($"Name of Attribute:{nameof(Person.name)}");
         }
-    }
 
+        // 1.4 Expression-bodied members example.
+        private static void displayTitle(string title) => Console.WriteLine($"\n{title} \n");
+
+        // 1.5 Exception filters example
+        private static void exceptionFilterExample()
+        {
+            int x = 54;
+            int y = 0;
+            try
+            {
+                int result = x / y;
+            }
+            catch (Exception ex) when (ex.Message.Contains("divide"))
+            {
+                Console.WriteLine($"Message2: {ex.Message}");
+            }
+        }
+
+        // 1.6 Index intializer
+        private static void indexInitializer()
+        {
+            Dictionary<int, string> intializedList = new Dictionary<int, string>
+            {
+                [1] = "Index one",
+                [2] = "Index two",
+                [3] = "Index three"
+            };
+
+            Console.WriteLine($"Index 1: {intializedList[1]}");
+            Console.WriteLine($"Index 2: {intializedList[2]}");
+            Console.WriteLine($"Index 3: {intializedList[3]}");
+        }
+
+        // 1.7 Using static
+        private static void usingStatic()
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine($"Sum a + b static {Calculator.sumStatic(a, b)}");
+            Console.WriteLine($"Sum a + b not static {new Calculator().sumNotStatic(a, b)}");
+        }
+
+        // 1.8 Auto property initializer
+        private static void autoPropertyInitializer()
+        {
+            InitializerExample init = new InitializerExample();
+            Console.WriteLine($"Default age: {init.age}");
+            Console.WriteLine($"Default address: {init.address}");
+        }
+    }
     public class Person
     {
         public string name { get; set; }
 
         public string address { get; set; }
+    }
+
+    public class Calculator
+    {
+        public static int sumStatic(int a, int b) => a + b;
+
+        public int sumNotStatic(int a, int b) => a + b;
+    }
+
+    public class InitializerExample
+    {
+        public int age { get; } = 10;
+
+        public string address { get; set; } = "Default Address";
     }
 
     // Indent shift + alt + f
